318b85b138be01727f8375fe882163362a7e498f
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-03-30 21:43:40 -0300

    Creacion de componentes React

---
+++ frontend/src/AgregarUsuario.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AgregarUsuarios(){
+    return(
+        <div>
+            <h2>Crear un nuevo usuario</h2>
+        </div>
+    )
+}
+
+export default AgregarUsuarios
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import ListaUsuarios from './ListaUsuarios';
+import AgregarUsuarios from './AgregarUsuario';
+import EditarUsuario from './EditarUsuario';
 
 function App() {
   return (
     <div className="App">
       <h1>Aplicacion de contactos MERN</h1>
+      <ListaUsuarios />
+      <AgregarUsuarios />
+      <EditarUsuario />
     </div>
   );
 }
